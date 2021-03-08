f7f861b0eb8a56d0f04907f006b2d6a720bdb80d
claud <180181@virtualwindow.co.za>
2021-03-08 12:30:03 +0200

    Add project files.

---
+++ Axolotl/EnterNamePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Axolotl
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EnterNamePage : ContentPage
+    {
+        public EnterNamePage()
+        {
+            InitializeComponent();
+        }
+
+        async void SavePlantClicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
